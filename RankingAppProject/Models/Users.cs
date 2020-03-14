56ff004db81d798bb51adde3dc76e197602be777
Aiganysh Talantbekova <ramonaramonr@gmail.com>
2020-03-14 17:15:31 +0600

    first commit(Aiganysh)

---
+++ RankingAppProject/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankingAppProject
+{
+    class User
+    {
+       public string Login;
+       public string Password;
+
+        public User(string _Login, string _Password)
+        {
+            Login = _Login;
+            Password = _Password;
+        }
+    }
+}
